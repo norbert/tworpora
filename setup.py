7b6e09ce4f3beee58fc715e82dbb5724104f9db4
Norbert Crombach <norbert.crombach@primetheory.org>
2014-02-21 15:53:11 +0100

    Configure setuptools

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(name='tworpora',
+      version='0.0.1',
+      packages=['tworpora'],
+      install_requires=['beautifulsoup4', 'python-twitter>=1.1',
+                        'joblib>=0.8.0a3'])
